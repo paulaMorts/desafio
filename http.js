b4c359e36c988106b3ba630553799848cb455f1c
Fernandes, Paula A <paula.a.fernandes@accenture.com>
2018-04-19 15:40:34 -0300

    first commit

---
+++ http.js
@@ -0,0 +1,25 @@
+var router = require('./router');
+
+var app = router(3212);
+
+var contatos = [
+	{nome: "Edjan Souza Santos", data: "1974-01-01", sexo: {nome: "Masculino", codigo: "M"}},
+	{nome: "Isabella Cacélia Vicente", data: "1996-05-20", sexo: {nome: "Feminino", codigo: "F"}},
+	{nome: "Elisabeth Ribeiro", data: "1992-07-14", sexo: {nome: "Feminino", codigo: "F"}},
+	{nome: "Paula Fernandes", data: "1991-03-22", sexo: {nome: "Feminino", codigo: "F"}}
+];
+
+var sexos = [
+	{nome: "Masculino", codigo: "M"},
+	{nome: "Feminino", codigo: "F"}
+];
+
+app.get('/sexos', function(req, res){
+	res.write(JSON.stringify(sexos));
+	res.end();
+});
+
+app.get('/contatos', function(req, res){
+	res.write(JSON.stringify(contatos));
+	res.end();
+});
---
+++ js/controllers/desafioCtrl.js
@@ -0,0 +1,46 @@
+angular.module("desafio").controller("desafioCtrl", function ($scope, $http) {
+				$scope.app = "Desafio";
+				$scope.contatos = [];
+				$scope.sexos = [];
+
+				var carregarContatos = function() {
+					$http.get("http://localhost:3212/contatos").then(function(data){
+						$scope.contatos = data.data;
+					});
+				};
+				var carregarSexos = function() {
+					$http.get("http://localhost:3212/sexos").then(function(data){
+						$scope.sexos = data.data;
+					});;
+				};
+				$scope.adicionarContato = function(contato) {
+					$scope.contatos.push(angular.copy(contato));
+					delete $scope.contato;
+					$scope.contatoForm.$setPristine();
+					$scope.openForm = false;
+				};
+				$scope.cancelarCadasro = function(contato) {
+					delete $scope.contato;
+					$scope.openForm = false;
+				};
+				$scope.ordenarPor = function(campo) {
+					$scope.criterioBusca = "";
+					$scope.criterioOrdenacao = campo;
+					$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+				};
+
+				$scope.iconGender = function(texto){
+					console.log(texto);
+					if (texto=="Masculino"){
+						texto = "M";
+					}
+					else 
+						texto="F";
+				}
+
+				$scope.classe1 = "selecionado";
+				$scope.classe2 = "negrito";
+
+				carregarContatos();
+				carregarSexos();
+			});
